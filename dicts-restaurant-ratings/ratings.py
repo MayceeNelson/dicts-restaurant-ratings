80b7eab1cdb732253e30a22d4acf96c0599ebb97
Maycee Nelson <Mayceenelson@yahoo.com>
2023-09-05 18:10:12 -0600

    sorted ratings

--- dicts-restaurant-ratings/ratings.py
+++ dicts-restaurant-ratings/ratings.py
@@ -1,4 +1,30 @@
 """Restaurant rating lister."""
 
 
-# put your code here
+def rating_lister(filename):
+    restaurants = []
+    file = open(filename)
+
+    for restaurant in file:
+        data = restaurant.rstrip().split(":")
+        data_tuple = tuple(data)
+        restaurants.append(data_tuple)
+    file.close()
+    
+    return restaurants
+
+
+def sorted_ratings():
+    ratings = {}
+    data = rating_lister("scores.txt")
+
+    for restaurant in data:
+        ratings[restaurant] = ratings.get(restaurant)
+  
+    # for item in ratings.items():
+    #     print(item)
+    return ratings
+
+
+print(sorted(sorted_ratings()))
+
